6d297aaa6eb1475b5ae93246062f4f8fc3c3994f
Tomoyuki Fujimori <moyu@dromozoa.com>
2022-06-24 14:01:40 +0900

    リファクタリング。

--- main.cpp
+++ main.cpp
@@ -16,7 +16,6 @@
 // along with dromozoa-web.  If not, see <http://www.gnu.org/licenses/>.
 
 #include <emscripten.h>
-#include <emscripten/fetch.h>
 #include <chrono>
 #include <iostream>
 #include <exception>
@@ -28,14 +27,10 @@ extern "C" int luaopen_dromozoa(lua_State*);
 
 class context_t {
 public:
-  context_t() : state_() {
-    state_ = luaL_newstate();
-    luaL_openlibs(state_);
-  }
+  context_t() : state_(luaL_newstate()) {}
 
   ~context_t() {
-    lua_close(state_);
-    state_ = nullptr;
+    close();
   }
 
   void load();
@@ -43,88 +38,82 @@ public:
 
 private:
   lua_State* state_;
+
+  void close() {
+    if (state_) {
+      lua_close(state_);
+      state_ = nullptr;
+    }
+  }
 };
 
-void download_succeeded(emscripten_fetch_t *fetch) {
-  std::cout << "download_succeeded " << fetch->numBytes << " " << fetch->url << "\n";
-  emscripten_fetch_close(fetch);
-}
-
-void download_failed(emscripten_fetch_t *fetch) {
-  std::cout << "download_succeeded " << fetch->status << " " << fetch->url << "\n";
-  emscripten_fetch_close(fetch);
-}
-
 void context_t::load() {
-  lua_State* L = state_;
-  luaL_openlibs(L);
+  try {
+    if (lua_State* L = state_) {
+      luaL_openlibs(L);
 
-  lua_getglobal(L, "package");
-  lua_getfield(L, -1, "preload");
-  lua_pushcfunction(L, luaopen_dromozoa);
-  lua_setfield(L, -2, "dromozoa");
-  lua_pop(L, 2);
+      lua_getglobal(L, "package");
+      lua_getfield(L, -1, "preload");
+      lua_pushcfunction(L, luaopen_dromozoa);
+      lua_setfield(L, -2, "dromozoa");
+      lua_pop(L, 2);
 
-  static const char code[] =
-  #include "main.lua"
-  ;
+      static const char code[] =
+      #include "main.lua"
+      ;
 
-  if (luaL_loadbuffer(L, code, strlen(code), "=(load)") != 0) {
-    throw DROMOZOA_LOGIC_ERROR("could not luaL_loadbuffer: ", lua_tostring(L, -1));
+      if (luaL_loadbuffer(L, code, strlen(code), "=(load)") != LUA_OK) {
+        throw DROMOZOA_LOGIC_ERROR("cannot luaL_loadbuffer: ", lua_tostring(L, -1));
+      }
+      if (lua_pcall(L, 0, 1, 0) != LUA_OK) {
+        throw DROMOZOA_LOGIC_ERROR("cannot lua_pcall: ", lua_tostring(L, -1));
+      }
+    }
+  } catch (...) {
+    dromozoa::push_exception_queue();
   }
-  lua_pcall(L, 0, 1, 0);
-
-/*
-  emscripten_fetch_attr_t attr;
-  emscripten_fetch_attr_init(&attr);
-  memcpy(attr.requestMethod, "GET", 4);
-  attr.attributes = EMSCRIPTEN_FETCH_LOAD_TO_MEMORY;
-  attr.onsuccess = download_succeeded;
-  attr.onerror = download_failed;
-
-  using clock_t = std::chrono::system_clock;
-  clock_t::time_point t = clock_t::now();
-
-  std::ostringstream out;
-  out << "README.md?t=" << clock_t::to_time_t(t);
-  emscripten_fetch(&attr, out.str().c_str());
-*/
-
-  std::cout << "load done\n";
 }
 
 void context_t::each() {
+  bool error = false;
+
   while (std::exception_ptr eptr = dromozoa::pop_exception_queue()) {
+    error = true;
     try {
       std::rethrow_exception(eptr);
     } catch (const std::runtime_error& e) {
-      std::cerr << "runtime_error " << e.what() << "\n";
+      std::cerr << "std::runtime_error " << e.what() << "\n";
     } catch (const std::exception& e) {
-      std::cerr << "exception " << e.what() << "\n";
+      std::cerr << "std::exception " << e.what() << "\n";
     } catch (...) {
       std::cerr << "unknown exception\n";
     }
   }
 
-  lua_State* L = state_;
-  if (lua_type(L, -1) == LUA_TFUNCTION) {
-    lua_pushvalue(L, -1);
-    lua_pcall(L, 0, 0, 0);
+  if (error) {
+    close();
+  }
+
+  try {
+    if (auto* L = state_) {
+      lua_pushvalue(L, -1);
+      if (lua_pcall(L, 0, 0, 0) != LUA_OK) {
+        throw DROMOZOA_LOGIC_ERROR("canot lua_pcall: ", lua_tostring(L, -1));
+      }
+    }
+  } catch (...) {
+    dromozoa::push_exception_queue();
   }
 }
 
 void each(void* data) {
-  context_t* context = static_cast<context_t*>(data);
+  auto* context = static_cast<context_t*>(data);
   context->each();
 }
 
 int main() {
-  context_t* context = new context_t();
-  try {
-    context->load();
-    emscripten_set_main_loop_arg(each, context, 30, true);
-  } catch (const std::exception& e) {
-    std::cerr << e.what() << "\n";
-  }
+  auto* context = new context_t();
+  context->load();
+  emscripten_set_main_loop_arg(each, context, 30, true);
   return 0;
 }

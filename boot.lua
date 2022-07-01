2a12d38d48778a96a5a851490de6665a994c5177
Tomoyuki Fujimori <moyu@dromozoa.com>
2022-07-01 20:14:50 +0900

    boot.luaを修正。

--- boot.lua
+++ boot.lua
@@ -21,57 +21,68 @@ local D = require "dromozoa.web"
 local prototype = D.window.Promise.prototype
 prototype.then_ = prototype["then"]
 
-local boot_thread = coroutine.create(function ()
-  local filename = D.new(D.window.URLSearchParams, D.window.document.location.search):get("main")
+local thread = coroutine.create(function ()
+  local window = D.window
+  local document = window.document
+
+  local filename = D.new(window.URLSearchParams, document.location.search):get "main"
   if not filename or filename == D.null then
     filename = "main.lua"
   end
-  local chunk
-  local error_message
 
-  D.window:fetch(filename, { cache = "no-store" })
-  :then_(function (response)
-    if response.ok then
-      return response:text()
-    else
-      D.throw(D.new(D.window.Error, response.statusText))
-    end
-  end)
-  :then_(function (text)
-    chunk = assert(load(text, filename))
-    done = true
-  end)
-  :catch(function (e)
-    print("error ", e.message)
-    done = true
-    error_message = e.message
-  end)
+  local result
+  local data
+
+  window:fetch(filename, { cache = "no-store" })
+    :then_(function (response)
+      if response.ok then
+        return response:text()
+      else
+        D.throw(("%d %s"):format(response.status, response.statusText))
+      end
+    end)
+    :then_(function (text)
+      result = true
+      data = text
+    end)
+    :catch(function (e)
+      result = false
+      data = e.message
+    end)
 
   while true do
-    if done then
+    while true do
+      local e = D.get_error()
+      if not e then
+        break
+      end
+      io.stderr:write(e, "\n")
+    end
+    if result ~= nil then
       break
     end
     coroutine.yield()
   end
 
-  if chunk then
-    return chunk()
-  else
-    error(error_message)
+  if not result then
+    error(data)
   end
+
+  return assert(load(data, filename))()
 end)
 
 return function ()
-  if not boot_thread then
+  if not thread then
     return
   end
 
-  local result, message = coroutine.resume(boot_thread)
-  if not result then
-    io.stderr:write("boot thread error: ", message, "\n")
+  local result, data = coroutine.resume(thread)
+  if coroutine.status(thread) == "dead" then
+    thread = nil
   end
-  if coroutine.status(boot_thread) == "dead" then
-    boot_thread = nil
+
+  if not result then
+    error(data)
   end
 end
 --)"--"

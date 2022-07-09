1aa59ba8f047d795ab1bf973088fde46550effeb
Tomoyuki Fujimori <moyu@dromozoa.com>
2022-07-09 10:57:27 +0900

    リファクタリング。

--- test/test_aws.lua
+++ test/test_aws.lua
@@ -16,8 +16,7 @@
 -- along with dromozoa-web.  If not, see <http://www.gnu.org/licenses/>.
 
 local D = require "dromozoa.web"
-local async = require "dromozoa.web.async"
-local await = async.await
+local async, await = require "dromozoa.web.async" .import "await"
 
 local window = D.window
 local crypto = window.crypto

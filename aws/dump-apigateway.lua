4f7de9e2c8e8843ca5ef6a123bf0de442e5b181b
Tomoyuki Fujimori <moyu@dromozoa.com>
2022-07-13 11:54:26 +0900

    スクリプトを修正。

--- aws/dump-apigateway.lua
+++ aws/dump-apigateway.lua
@@ -1,26 +1,85 @@
 #! /usr/bin/env lua
 
+-- Copyright (C) 2022 Tomoyuki Fujimori <moyu@dromozoa.com>
+--
+-- This file is part of dromozoa-web.
+--
+-- dromozoa-web is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+--
+-- dromozoa-web is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with dromozoa-web.  If not, see <http://www.gnu.org/licenses/>.
+
 local json = require "dromozoa.commons.json"
 
 local function run(command)
+  io.stderr:write(command, "\n")
   local handle = io.popen(command, "r")
   local content = handle:read "*a"
   assert(handle:close())
   return assert(json.decode(content))
 end
 
+local function run_apigateway(command)
+  return run("aws apigateway " .. command)
+end
+
 local function run_apigatewayv2(command)
   return run("aws apigatewayv2 " .. command)
 end
 
-local api_names = {
+local result = {}
+
+local names = {
+  "authorizers";
+  "deployments";
+  "documentation-parts";
+  "documentation-versions";
+  "gateway-responses";
+  "models";
+  "request-validators";
+  "resources";
+  "stages";
+}
+
+local root = run_apigateway "get-rest-apis"
+for _, item in ipairs(root.items) do
+  local rest_api_id = assert(item.id)
+  for _, name in ipairs(names) do
+    local data = run_apigateway("get-" .. name .. " --rest-api-id " .. rest_api_id)
+    item[name] = data
+    if name == "models" then
+      for _, model in ipairs(data.items) do
+        local arg = " --rest-api-id " .. rest_api_id .. " --model-name " .. model.name
+        model.model_template = run_apigateway("get-model-template" .. arg)
+      end
+    elseif name == "resources" then
+      for _, resource in ipairs(data.items) do
+        local resource_id = resource.id
+        for http_method, method in pairs(resource.resourceMethods or {}) do
+          local arg = " --rest-api-id " .. rest_api_id .. " --resource-id " .. resource_id .. " --http-method " .. http_method
+          method.integration = run_apigateway("get-integration" .. arg)
+          -- get-integration-responseは不要
+        end
+      end
+    end
+  end
+end
+result.v1 = root
+
+local names = {
   "authorizers";
   "deployments";
   "integrations";
-  -- "integration-responses";
   "models";
   "routes";
-  -- "route-responses";
   "stages";
 }
 
@@ -37,24 +96,23 @@ local responses = {
   };
 }
 
-local apis = run_apigatewayv2 "get-apis"
-for _, item in ipairs(apis.Items) do
+local root = run_apigatewayv2 "get-apis"
+for _, item in ipairs(root.Items) do
   local api_id = assert(item.ApiId)
-  for _, api_name in ipairs(api_names) do
-    local command = "get-" .. api_name .. " --api-id " .. api_id
-    io.stderr:write("run ", command, "\n")
-    local data = run_apigatewayv2(command)
-    item[api_name] = data
-    local response = responses [api_name]
+  for _, name in ipairs(names) do
+    local data = run_apigatewayv2("get-" .. name .. " --api-id " .. api_id)
+    item[name] = data
+    local response = responses[name]
     if response then
-      local response_name = response.name
+      local opt = response.opt
+      local ref = response.ref
+      local name = response.name
       for _, item in ipairs(data.Items) do
-        local command = "get-" .. response_name .. " --api-id " .. api_id .. " " .. response.opt .. " " .. item[response.ref]
-        io.stderr:write("run ", command, "\n")
-        item[response_name] = run_apigatewayv2(command)
+        item[name] = run_apigatewayv2("get-" .. name .. " --api-id " .. api_id .. " " .. opt .. " " .. item[ref])
       end
     end
   end
 end
+result.v2 = root
 
-print(json.encode(apis, { pretty = true, stable = true }))
+print(json.encode(result, { pretty = true, stable = true }))
